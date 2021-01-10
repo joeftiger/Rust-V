a3a162fdd6dbbb86d3af83911b7379221319a9a6
Julius Oeftiger <j.oeftiger@protonmail.com>
2021-01-10 20:43:32 +0100

    Remove lazy static conf, convert to function

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,8 +3,6 @@ extern crate clap;
 
 use clap::App;
 
-use lazy_static::lazy_static;
-
 use rust_v::demo_scenes::{CornellScene, DebugScene, DemoScene, SphereScene};
 use rust_v::integrator::{DebugNormals, Integrator, Path, Whitted};
 use rust_v::renderer::Renderer;
@@ -32,103 +30,101 @@ const FORMAT: &str = "FORMAT";
 const INTEGRATOR_BACKEND: &str = "INTEGRATOR_BACKEND";
 const THREADS: &str = "THREADS";
 
-lazy_static! {
-    static ref MAIN_CONFIG: MainConfig = {
-        #[cfg(not(feature = "live-window"))]
-        let yaml = load_yaml!("cli.yml");
-        #[cfg(feature = "live-window")]
-        let yaml = load_yaml!("cli-live.yml");
-
-        let app_matches = App::from_yaml(yaml).get_matches();
-        let demo = if let Some(spheres) = app_matches.subcommand_matches(SPHERE_SCENE) {
-            (spheres, DemoType::SphereScene)
-        } else if let Some(cornell) = app_matches.subcommand_matches(CORNELL_SCENE) {
-            (cornell, DemoType::CornellScene)
-        } else if let Some(debug) = app_matches.subcommand_matches(DEBUG_SCENE) {
-            (debug, DemoType::DebugScene)
-        } else {
-            panic!("Currently we only support the subcommands (spheres, cornell, debug)!");
-        };
-
-        let matches = demo.0;
-        let verbose = matches.is_present(VERBOSE);
-
-        let width = match matches.value_of(WIDTH).unwrap_or("900").parse() {
-            Ok(width) => width,
-            Err(err) => panic!("Cannot parse width: {}", err),
-        };
-        let height = match matches.value_of(HEIGHT).unwrap_or("900").parse() {
-            Ok(height) => height,
-            Err(err) => panic!("Cannot parse height: {}", err),
-        };
-        let depth = match matches.value_of(DEPTH).unwrap_or("6").parse() {
-            Ok(depth) => depth,
-            Err(err) => panic!("Cannot parse depth: {}", err),
-        };
-        let passes = match matches.value_of(PASSES).unwrap_or("1").parse() {
-            Ok(passes) => passes,
-            Err(err) => panic!("Cannot parse passes: {}", err),
-        };
-        let block_size = match matches.value_of(BLOCK_SIZE).unwrap_or("8").parse() {
-            Ok(block_size) => block_size,
-            Err(err) => panic!("Cannot parse block size: {}", err),
-        };
-        let live = cfg!(feature = "live-window") && matches.is_present(LIVE);
-        let threads = match matches
-            .value_of(THREADS)
-            .unwrap_or(&num_cpus::get().to_string())
-            .parse()
-        {
-            Ok(threads) => threads,
-            Err(err) => panic!("Cannot parse threads: {}", err),
-        };
-        let pixel_type: PixelType = match matches.value_of(FORMAT).unwrap_or("u8").try_into() {
-            Ok(format) => format,
-            Err(err) => panic!("Cannot parse pixel format: {}", err),
-        };
-        let integrator_type: IntegratorType = match matches
-            .value_of(INTEGRATOR_BACKEND)
-            .unwrap_or("whitted")
-            .try_into()
-        {
-            Ok(integrator) => integrator,
-            Err(err) => panic!("Cannot parse integrator backend: {}", err),
-        };
-
-        let output = if let Some(o) = matches.value_of(OUTPUT) {
-            o.to_string()
-        } else {
-            format!(
-                "{}.png",
-                std::time::SystemTime::now()
-                    .duration_since(std::time::UNIX_EPOCH)
-                    .unwrap()
-                    .as_millis()
-            )
-        };
-        let output = if output.is_empty() {
-            None
-        } else {
-            Some(output)
-        };
-
-        let demo_type = demo.1;
-        let render_config = RenderConfig::new(width, height, depth, passes, block_size, threads);
-
-        MainConfig {
-            render_config,
-            verbose,
-            live,
-            output,
-            pixel_type,
-            integrator_type,
-            demo_type,
-        }
-    };
+fn main() -> Result<(), String> {
+    create_config().run()
 }
 
-fn main() -> Result<(), String> {
-    MAIN_CONFIG.run()
+fn create_config() -> MainConfig {
+    #[cfg(not(feature = "live-window"))]
+    let yaml = load_yaml!("cli.yml");
+    #[cfg(feature = "live-window")]
+    let yaml = load_yaml!("cli-live.yml");
+
+    let app_matches = App::from_yaml(yaml).get_matches();
+    let demo = if let Some(spheres) = app_matches.subcommand_matches(SPHERE_SCENE) {
+        (spheres, DemoType::SphereScene)
+    } else if let Some(cornell) = app_matches.subcommand_matches(CORNELL_SCENE) {
+        (cornell, DemoType::CornellScene)
+    } else if let Some(debug) = app_matches.subcommand_matches(DEBUG_SCENE) {
+        (debug, DemoType::DebugScene)
+    } else {
+        panic!("Currently we only support the subcommands (spheres, cornell, debug)!");
+    };
+
+    let matches = demo.0;
+    let verbose = matches.is_present(VERBOSE);
+
+    let width = match matches.value_of(WIDTH).unwrap_or("900").parse() {
+        Ok(width) => width,
+        Err(err) => panic!("Cannot parse width: {}", err),
+    };
+    let height = match matches.value_of(HEIGHT).unwrap_or("900").parse() {
+        Ok(height) => height,
+        Err(err) => panic!("Cannot parse height: {}", err),
+    };
+    let depth = match matches.value_of(DEPTH).unwrap_or("6").parse() {
+        Ok(depth) => depth,
+        Err(err) => panic!("Cannot parse depth: {}", err),
+    };
+    let passes = match matches.value_of(PASSES).unwrap_or("1").parse() {
+        Ok(passes) => passes,
+        Err(err) => panic!("Cannot parse passes: {}", err),
+    };
+    let block_size = match matches.value_of(BLOCK_SIZE).unwrap_or("8").parse() {
+        Ok(block_size) => block_size,
+        Err(err) => panic!("Cannot parse block size: {}", err),
+    };
+    let live = cfg!(feature = "live-window") && matches.is_present(LIVE);
+    let threads = match matches
+        .value_of(THREADS)
+        .unwrap_or(&num_cpus::get().to_string())
+        .parse()
+    {
+        Ok(threads) => threads,
+        Err(err) => panic!("Cannot parse threads: {}", err),
+    };
+    let pixel_type: PixelType = match matches.value_of(FORMAT).unwrap_or("u8").try_into() {
+        Ok(format) => format,
+        Err(err) => panic!("Cannot parse pixel format: {}", err),
+    };
+    let integrator_type: IntegratorType = match matches
+        .value_of(INTEGRATOR_BACKEND)
+        .unwrap_or("whitted")
+        .try_into()
+    {
+        Ok(integrator) => integrator,
+        Err(err) => panic!("Cannot parse integrator backend: {}", err),
+    };
+
+    let output = if let Some(o) = matches.value_of(OUTPUT) {
+        o.to_string()
+    } else {
+        format!(
+            "{}.png",
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_millis()
+        )
+    };
+    let output = if output.is_empty() {
+        None
+    } else {
+        Some(output)
+    };
+
+    let demo_type = demo.1;
+    let render_config = RenderConfig::new(width, height, depth, passes, block_size, threads);
+
+    MainConfig {
+        render_config,
+        verbose,
+        live,
+        output,
+        pixel_type,
+        integrator_type,
+        demo_type,
+    }
 }
 
 #[derive(Debug, Clone)]
