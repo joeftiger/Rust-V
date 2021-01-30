db209c466484f5fd4391c3936c886079de673321
Julius Oeftiger <j.oeftiger@protonmail.com>
2021-01-30 19:22:47 +0100

    Remove redundant paranthesis

--- src/bxdf/oren_nayar.rs
+++ src/bxdf/oren_nayar.rs
@@ -47,28 +47,25 @@ impl BxDF for OrenNayar {
         let sin_theta_i = sin_theta(incident);
         let sin_theta_o = sin_theta(outgoing);
 
-        let max_cos = {
-            if sin_theta_i > EPSILON && sin_theta_o > EPSILON {
-                let sin_phi_i = sin_phi(incident);
-                let sin_phi_o = sin_phi(outgoing);
-                let cos_phi_i = cos_phi(incident);
-                let cos_phi_o = cos_phi(outgoing);
+        let max_cos = if sin_theta_i > EPSILON && sin_theta_o > EPSILON {
+            let sin_phi_i = sin_phi(incident);
+            let sin_phi_o = sin_phi(outgoing);
+            let cos_phi_i = cos_phi(incident);
+            let cos_phi_o = cos_phi(outgoing);
 
-                let d_cos = cos_phi_i * cos_phi_o + sin_phi_i * sin_phi_o;
-                d_cos.max(0.0)
-            } else {
-                0.0
-            }
+            let d_cos = cos_phi_i * cos_phi_o + sin_phi_i * sin_phi_o;
+            d_cos.max(0.0)
+        } else {
+            0.0
         };
 
         let cos_theta_i_abs = cos_theta(incident).abs();
         let cos_theta_o_abs = cos_theta(outgoing).abs();
-        let (sin_alpha, tan_beta) = {
-            if cos_theta_i_abs > cos_theta_o_abs {
-                (sin_theta_o, sin_theta_i / cos_theta_i_abs)
-            } else {
-                (sin_theta_i, sin_theta_o / cos_theta_o_abs)
-            }
+
+        let (sin_alpha, tan_beta) = if cos_theta_i_abs > cos_theta_o_abs {
+            (sin_theta_o, sin_theta_i / cos_theta_i_abs)
+        } else {
+            (sin_theta_i, sin_theta_o / cos_theta_o_abs)
         };
 
         self.r * (FRAC_1_PI * (self.a + self.b * max_cos * sin_alpha * tan_beta))
