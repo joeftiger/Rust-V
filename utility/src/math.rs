066f10d54233f3a945122abec24218281073adbe
Julius Oeftiger <>
2021-06-11 16:22:24 +0200

    Add error function

--- utility/src/math.rs
+++ utility/src/math.rs
@@ -75,3 +75,61 @@ pub fn sinc(mut x: Float) -> Float {
         Float::sin(pix) / pix
     }
 }
+
+#[allow(clippy::excessive_precision)]
+#[inline]
+#[must_use]
+pub fn erf(x: Float) -> Float {
+    // constants
+    let a1 = 0.254829592;
+    let a2 = -0.284496736;
+    let a3 = 1.421413741;
+    let a4 = -1.453152027;
+    let a5 = 1.061405429;
+    let p = 0.3275911;
+
+    // Save the sign of x
+    let sign = x.signum();
+    let x = x.abs();
+
+    // A&S formula 7.1.26
+    let t = 1.0 / (1.0 + p * x);
+    let y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Float::exp(-x * x);
+
+    sign * y
+}
+
+#[allow(clippy::excessive_precision)]
+#[inline]
+#[must_use]
+pub fn erf_inv(x: Float) -> Float {
+    let x = Float::fast_clamp(x, -1.0 + Float::big_epsilon(), 1.0 - Float::big_epsilon());
+
+    let mut w = -Float::ln((1.0 - x) * (1.0 + x));
+    let mut p: Float;
+    if w < 5.0 {
+        w -= 2.5;
+        p = 2.81022636e-08;
+        p = 3.43273939e-07 + p * w;
+        p = -3.5233877e-06 + p * w;
+        p = -4.39150654e-06 + p * w;
+        p = 0.00021858087 + p * w;
+        p = -0.00125372503 + p * w;
+        p = -0.00417768164 + p * w;
+        p = 0.246640727 + p * w;
+        p = 1.50140941 + p * w;
+    } else {
+        w = w.sqrt() - 3.0;
+        p = -0.000200214257;
+        p = 0.000100950558 + p * w;
+        p = 0.00134934322 + p * w;
+        p = -0.00367342844 + p * w;
+        p = 0.00573950773 + p * w;
+        p = -0.0076224613 + p * w;
+        p = 0.00943887047 + p * w;
+        p = 1.00167406 + p * w;
+        p = 2.83297682 + p * w;
+    }
+
+    p * x
+}
